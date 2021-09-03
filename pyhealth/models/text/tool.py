d115895d30bf7ed288fcd9c9124618e5c21bd22e
Tim as short <49252667+Abhinav43@users.noreply.github.com>
2021-09-03 16:31:58 +0530

    Update tool.py

--- pyhealth/models/text/tool.py
+++ pyhealth/models/text/tool.py
@@ -10,27 +10,25 @@ import os
 
 def get_embedding(embed_type):
     if embed_type == 'BioBERT':
-        model_loc = './auxiliary/pretrained_bert_tf/biobert_pretrain_output_all_notes_150000/'
+        model_loc = '/content/drive/MyDrive/models_a/pretrained_bert_tf/biobert_pretrain_output_all_notes_150000/'
         tokenizer = BertTokenizer.from_pretrained(model_loc, do_lower_case=True)
         cache_dir = os.path.join(PYTORCH_PRETRAINED_BERT_CACHE, 'distributed_{}'.format(-1))
         model = BertModel.from_pretrained(model_loc, cache_dir=cache_dir)
         indexer = None
     elif embed_type == 'BERT':
-        model_loc = './auxiliary/pretrained_bert_tf/bert_pretrain_output_all_notes_150000/'
+        model_loc = '/content/drive/MyDrive/models_a/pretrained_bert_tf/bert_pretrain_output_all_notes_150000/'
         tokenizer = BertTokenizer.from_pretrained(model_loc, do_lower_case=True)
         cache_dir = os.path.join(PYTORCH_PRETRAINED_BERT_CACHE, 'distributed_{}'.format(-1))
         model = BertModel.from_pretrained(model_loc, cache_dir=cache_dir)
         indexer = None
     elif embed_type == 'CharBERT':
-        model_loc = './auxiliary/pretrained_character_bert/general_character_bert/'
+        model_loc = '/content/drive/MyDrive/models_a/general_character_bert/'
         model = CharacterBertModel.from_pretrained(model_loc)
-        tokenizer = BertTokenizer.from_pretrained('./auxiliary/pretrained_bert_tf/bert_pretrain_output_all_notes_150000/')
+        tokenizer = BertTokenizer.from_pretrained('/content/drive/MyDrive/models_a/pretrained_bert_tf/bert_pretrain_output_all_notes_150000/')
         indexer = CharacterIndexer() 
     elif embed_type == 'BioCharBERT':
-        model_loc = './auxiliary/pretrained_character_bert/medical_character_bert/'
+        model_loc = '/content/drive/MyDrive/models_a/medical_character_bert/'
         model = CharacterBertModel.from_pretrained(model_loc)
-        tokenizer = BertTokenizer.from_pretrained('./auxiliary/pretrained_bert_tf/biobert_pretrain_output_all_notes_150000/')
+        tokenizer = BertTokenizer.from_pretrained('/content/drive/MyDrive/models_a/pretrained_bert_tf/biobert_pretrain_output_all_notes_150000/')
         indexer = CharacterIndexer()        
     return indexer, tokenizer, model
-
-
